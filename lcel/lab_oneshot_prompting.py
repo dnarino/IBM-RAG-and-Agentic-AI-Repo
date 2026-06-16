afa0525c1c06bcb035e06f9d9f8c4e20e5a867e2
dnarino <dnarino>
2026-06-16 18:31:09 -0500

    Update oneshot prompting lab with JSON, Email, and Technical concepts

--- lcel/lab_oneshot_prompting.py
+++ lcel/lab_oneshot_prompting.py
@@ -11,21 +11,19 @@ from core.watson_client import get_watsonx_llm
 
 # 1. Get the model engine
 params = {
-        "max_new_tokens": 20,
+        "max_new_tokens": 150,
         "temperature": 0.1,
     }
 llm = get_watsonx_llm(params)
 
 # 2. Write a One-shot prompt
-prompt_txt = """Here is an example of translating a sentence from English to French:
+prompt_txt = """Return the latitude and longitude of the requested city in JSON format.
 
-            English: “How is the weather today?”
-            French: “Comment est le temps aujourd'hui?”
-            
-            Now, translate the following sentence from English to French:
-            
-            English: “Where is the nearest supermarket?”
-            French: """
+City: Paris
+JSON: {"latitude": 48.8566, "longitude": 2.3522}
+
+City: Bogota
+JSON: """
 
 # 3. Invoke and print!
 print("=" * 40)
@@ -39,3 +37,49 @@ print("Sending prompt to Watson...\n")
 response = llm.invoke(prompt_txt)
 print(response)
 print("=" * 40)
+
+## Starter code: provide your solutions in the TODO parts
+
+# 1. One-shot prompt for formal email writing
+formal_email_prompt = """Write a formal corporate email based on the provided topic.
+
+Topic: Request a meeting with the marketing team to discuss the Q3 campaign.
+Email:
+Subject: Request for Meeting: Q3 Campaign Discussion
+
+Dear Marketing Team,
+
+I hope this email finds you well. I would like to request a meeting to discuss our upcoming\
+Q3 marketing campaign. Please let me know your availability for next week so we can schedule\
+a time to align our strategies.
+
+Best regards,
+[Your Name]
+
+Topic: Announce that the office will be closed tomorrow because we wiil have been drinking beer \
+and watching the colombia soccer match.
+Email:
+"""
+
+# 2. One-shot prompt for simplifying technical concepts
+technical_concept_prompt = """Simplify a technical concept into a brief summary of no more than \
+50 words.
+
+Topic: API (Application Programming Interface)
+Concept: An API is a software bridge that allows different applications to talk to each other. \
+Like a waiter in a restaurant, it takes your request to the system and brings the response back to you.
+
+Topic: New Fabric AI model
+Concept:
+"""
+
+
+promts= {
+    "formal_email": formal_email_prompt,
+    "technical_concept": technical_concept_prompt
+}
+
+for prompt_index, promt_text in promts.items():
+    print(f"=== {prompt_index.upper()} RESPONSE ===")
+    response = llm.invoke(promt_text)
+    print(response)
