7b95cd66baa38048794405891b461458aa7096b7
dnarino <dnarino>
2026-06-20 11:28:17 -0500

    feat(core): configure global telemetry disabling and warning suppression in init

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,12 @@
 # Init core module
+import os
+import warnings
+
+# Disable LangChain telemetry globally
+os.environ['ANONYMIZED_TELEMETRY'] = 'False'
+
+# Suppress annoying warnings globally
+def warn(*args, **kwargs):
+    pass
+warnings.warn = warn
+warnings.filterwarnings('ignore')
